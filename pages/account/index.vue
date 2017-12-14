8de0b7d87e1eff1e1794810b79fba98768fe00ca
pvenableh <peter@huestudios.com>
2017-12-14 09:48:47 -0500

    Updated account access.

--- pages/account/index.vue
+++ pages/account/index.vue
@@ -1,12 +1,14 @@
 
 <template>
   <div class="grid-x account-container">
-   <h1 class="small-12 cell text-center text-lowercase account__title"><img :src="loggedUser.picture" style="width: 50px; height: 50px;"/> <span class="green">{{ loggedUser.given_name}}'s</span> account</h1>
-    
+   <h1 class="small-12 cell text-center text-lowercase account__title"><img :src="loggedUser.picture" style="width: 50px; height: 50px;"/> <span class="green" v-if="loggedUser.given_name">{{ loggedUser.given_name}}'s</span> account</h1>
+
    <form class="small-12 cell">
   <div class="grid-x account__user">
   <p class="small-12 medium-10 medium-offset-1 text-center cell account__user-intro">hey <span class="green">{{loggedUser.given_name}}</span>! <br></br> this is your camila.life profile.  we will be updating this to help customize the experience and track your journey when using our app.  so, make sure it's accurate and keep enjoying the <i class="fa fa-heart red"></i>!!</p>
-
+    <div id="social-notice" class="small-10 medium-8 small-offset-1 medium-offset-2 text-center text-uppercase cell black" v-if="this.$store.state.user.identities[0].isSocial">
+      <h5 style="color: black">Your account credentials are managed through the social account you logged in with.</h5>
+    </div>
     <div class="small-3 cell">
       <label for="middle-label" class="text-right middle">Email</label>
     </div>
@@ -17,16 +19,22 @@
     <div class="small-3 cell">
       <label for="middle-label" class="text-right middle">first name</label>
     </div>
-    <div class="small-9 medium-6 large-6 cell">
-      <input type="text" id="middle-label" placeholder="" :value="given_name" v-model="given_name">
+    <div class="small-9 medium-6 large-6 cell required-box">
+      <input type="text" id="middle-label" placeholder="" :value="given_name" v-model="given_name" v-if="this.$store.state.user.identities[0].isSocial" readonly>
+      <input type="text" id="middle-label" placeholder="" :value="given_name" v-model="given_name" v-else required>
+      <div id="given-name-error" class="error">REQUIRED</div>
     </div>
     <div class="small-12 cell show-for-medium"></div>
     <div class="small-3 cell">
       <label for="middle-label" class="text-right middle">last name</label>
     </div>
-    <div class="small-9 medium-6 large-6 cell">
-      <input type="text" id="middle-label" placeholder="" :value="family_name" v-model="family_name">
+
+    <div class="small-9 medium-6 large-6 cell required-box">
+      <input type="text" v-if="this.$store.state.user.identities[0].isSocial" id="middle-label" placeholder="" :value="family_name" v-model="family_name" readonly>
+      <input type="text" v-else id="middle-label" placeholder="" :value="family_name" v-model="family_name" required>
+      <div id="family-name-error" class="error">REQUIRED</div>
     </div>
+
     <div class="small-12 cell show-for-medium"></div>
     <div class="small-3 cell">
       <label for="middle-label" class="text-right middle">birthday</label>
@@ -80,25 +88,49 @@ export default {
   scrollToTop: true,
   created () {
     const vm = this
+    console.log('is social? ' + this.$store.state.user.identities[0].isSocial)
     axios.get('https://huestudios.com/sites/camila.life/content/api/1.1/tables/users/rows/?filters[email][eq]=' + this.$store.state.user.email)
       .then((res) => {
         console.log(res)
+        var givenNameError = document.getElementById('given-name-error')
+        var familyNameError = document.getElementById('family-name-error')
+
+        if (!vm.given_name) {
+          vm.given_name = res.data.data[0].given_name
+        }
+        if (!vm.family_name) {
+          vm.family_name = res.data.data[0].family_name
+        }
         if (!vm.zip_code) {
           vm.zip_code = res.data.data[0].zip_code
         }
         if (!vm.birthday) {
           vm.birthday = res.data.data[0].birthday
         }
+        if (!vm.given_name) {
+          givenNameError.classList.add('fadeInDown')
+          console.log('no given name')
+        } else {
+          givenNameError.classList.remove('fadeInDown')
+        }
+        if (!vm.family_name) {
+          familyNameError.classList.add('fadeInDown')
+        } else {
+          familyNameError.classList.remove('fadeInDown')
+        }
+        console.log('THIS ' + vm.given_name)
       })
   },
   methods: {
     updateUser () {
-      axios.post('https://huestudios.com/sites/camila.life/scripts/user.php?email=' + this.email + '&given_name=' + this.given_name + '&family_name=' + this.family_name + '&birthday=' + this.birthday + '&zip_code=' + this.zip_code + '&table=users').then(res => {
-        console.log(res)
-      }).catch(function (error) {
-        console.log(error)
-      })
-      this.showSuccessMsg({title: `<span class='condensed' style='font-size: 20px; font-weight: 100;'>successully updated profile`, image: this.loggedUser.picture})
+      if (this.given_name && this.family_name) {
+        axios.post('https://huestudios.com/sites/camila.life/scripts/user.php?email=' + this.email + '&given_name=' + this.given_name + '&family_name=' + this.family_name + '&birthday=' + this.birthday + '&zip_code=' + this.zip_code + '&table=users').then(res => {
+          console.log(res)
+        }).catch(function (error) {
+          console.log(error)
+        })
+        this.showSuccessMsg({title: `<span class='condensed' style='font-size: 20px; font-weight: 100;'>successully updated profile`, image: this.loggedUser.picture})
+      }
     },
     getFirstWord (string) {
       var first = string.split(' ')
@@ -128,7 +160,7 @@ export default {
 .account-container {
   min-height:600px;
   max-width: 700px;
- 
+
   .account__title {
     margin-top: 40px;
     margin-bottom: 20px;
@@ -145,7 +177,7 @@ export default {
       border-radius: 50%;
       border: thin solid $green;
       margin-right: 15px;
-      
+
     }
   }
   .account__title span {
@@ -205,6 +237,7 @@ input {
   box-shadow: none;
   color: $white;
   font-family: $bold-font;
+  position: relative;
 }
 input:focus {
   border: none;
@@ -242,4 +275,19 @@ input:focus {
   }
 }
 
+.social-alert, .given-name-error, .family-name-error {
+  display: none;
+}
+.required-box {
+  position: relative;
+}
+.error {
+  font-family: $condensed-bold-font;
+  color: $green;
+  font-size: 14px;
+  text-align: right;
+  position: absolute;
+  bottom: 30px;
+  right: 0px;
+}
 </style>
--- pages/auth/signed-in.vue
+++ pages/auth/signed-in.vue
@@ -18,12 +18,16 @@ export default {
     }
     setToken(token)
     this.$store.commit('SET_USER', getUserFromLocalStorage())
-    axios.post('http://camila.life/scripts/user.php?table=users&name=' + this.$store.state.user.name + '&email=' + this.$store.state.user.email + '&given_name=' + this.$store.state.user.given_name + '&family_name=' + this.$store.state.user.family_name + '&email_verified=' + this.$store.state.user.email_verified + '&gender=' + this.$store.state.user.gender + '&birthday=' + this.$store.state.user.birthday + '&last_login=' + this.$store.state.user.updated_at + '&created_at=' + this.$store.state.user.created_at + '&time_zone=' + this.$store.state.user.timezone + '&picture=' + escape(this.$store.state.user.picture) + '&picture_large=' + escape(this.$store.state.user.picture_large) + '&identity=' + this.$store.state.user.identities[0].connection + '&social_id=' + this.$store.state.user.identities[0].user_id).then(function (response) {
+    axios.post('https://huestudios.com/sites/camila.life/scripts/user.php?table=users&name=' + this.$store.state.user.name + '&email=' + this.$store.state.user.email + '&given_name=' + this.$store.state.user.given_name + '&family_name=' + this.$store.state.user.family_name + '&email_verified=' + this.$store.state.user.email_verified + '&gender=' + this.$store.state.user.gender + '&birthday=' + this.$store.state.user.birthday + '&last_login=' + this.$store.state.user.updated_at + '&created_at=' + this.$store.state.user.created_at + '&time_zone=' + this.$store.state.user.timezone + '&picture=' + escape(this.$store.state.user.picture) + '&picture_large=' + escape(this.$store.state.user.picture_large) + '&identity=' + this.$store.state.user.identities[0].connection + '&social_id=' + this.$store.state.user.identities[0].user_id).then(function (response) {
       console.log(response)
     }).catch(function (error) {
       console.log(error)
     })
-    this.$router.replace('/')
+    if (this.$store.state.user.given_name) {
+      this.$router.replace('/')
+    } else {
+      this.$router.replace('/account')
+    }
   }
 }
 </script>
