2b803bd968845bf2d61b9ec9b07effb25c82020f
pvenableh <peter@huestudios.com>
2018-07-13 15:39:13 -0400

    Updated recipe directions.

--- components/recipeIngredient.vue
+++ components/recipeIngredient.vue
@@ -1,9 +1,9 @@
 <template>
-  <nuxt-link :to="url">
+  <nuxt-link :to="ingredient.url">
   <div class="grid-x recipe-ingredient__container">
 
     <div class="cell shrink recipe-ingredient__image">
-       <img v-if="image" :src="'https://huestudios.com' + image.thumbnail_url" :alt="ingredient.name" :name="ingredient.name"/>
+       
     </div>
     <h5 class="cell auto recipe-ingredient__description"><span class="recipe-ingredient__measurement">{{measurement}} {{label}}</span> {{ ingredient.name }} <span class="recipe-ingredient__text">{{description}}</span></h5>
 
@@ -21,9 +21,7 @@ export default {
     id: Number,
     measurement: String,
     label: String,
-    description: String,
-    image: '',
-    url: ''
+    description: String
   },
   data () {
     return {
@@ -38,26 +36,19 @@ export default {
     if (!vm.recipe) {
       axios.get('https://huestudios.com/sites/camila.life/content/api/1.1/tables/ingredients/rows/?filters[id][eq]=' + vm.id).then(response => {
         vm.ingredient = response.data.data[0]
-        vm.image = vm.ingredient.images.data[0]
-        vm.url = '/plant-based-vegan-products/' + vm.ingredient.url
       }).catch(e => {
         this.errors.push(e)
       })
     } else {
       axios.get('https://huestudios.com/sites/camila.life/content/api/1.1/tables/recipes/rows/?filters[id][eq]=' + vm.recipe).then(response => {
         vm.ingredient = response.data.data[0]
-        vm.image = vm.ingredient.images.data[0]
-        vm.url = '/vegan-plant-based-recipes/' + vm.ingredient.url
       }).catch(e => {
         this.errors.push(e)
       })
     }
   },
   methods: {
-    showLoginScreen () {
-      var loginForm = document.getElementById('login-screen')
-      loginForm.classList.remove('close')
-    }
+
   },
   notifications: {
     showSuccessMsg: {
@@ -85,33 +76,5 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-@import './assets/scss/_vars.scss';
-a {
 
-}
-.recipe-ingredient__container {
-  flex-direction: row;
-  align-content: center;
-  justify-content: center;
-  align-items: center;
-  color: $white;
-    .recipe-ingredient__image {
-      width: 50px;
-      img {
-        width: 50px;
-        height: 50px;
-        border-radius: 50%;
-        border: thin solid $green;
-      }
-    }
-
-    .recipe-ingredient__description {
-      margin-bottom: 0px;
-      margin-left: 5px;
-      color: $green;
-      .recipe-ingredient__measurement {
-        color: $white;
-      }
-    }
-}
 </style>
--- pages/vegan-plant-based-recipes/_slug/index.vue
+++ pages/vegan-plant-based-recipes/_slug/index.vue
@@ -7,67 +7,116 @@
   <span v-if="recipe.cuisine" class="pink"> {{recipe.cuisine}}</span></h3>
     <h1 class="pink condensed-bold">{{findLove(recipe.name)}}<span v-if="love"><br>LOVE</span></h1>
   </div>
-  <div class="uk-hidden@m recipe-title" uk-parallax="y: 0px,-30px; x: 0px, 15px; color: #1a1a1a, #fff; background-color: rgba(1,1,1,1); scale: 1, 0.7; target: .top-section; easing: 0.8;">
+  <div class="uk-hidden@m recipe-title" uk-parallax="y: 0px,-40px; x: 0px, 15px; color: #1a1a1a, #fff; background-color: rgba(1,1,1,1); scale: 1, 0.7; target: .top-section; easing: 0.8;">
     <h3><span v-if="style.length > 0">{{style[0]}} /</span>
   <span v-if="recipe.cuisine" class="pink"> {{recipe.cuisine}}</span></h3>
     <h1 class="pink condensed-bold">{{findLove(recipe.name)}}<span v-if="love"><br>LOVE</span></h1>
   </div>
 
-  <div class="uk-width-1-1 uk-position-relative uk-visible-toggle uk-light top-section recipe-images" uk-slider="center: true; animation: pull" v-if="images.length > 0">
-    <ul class="uk-slider-items uk-grid">
-      <li class="slider-item uk-width-1-1" v-for="(image, index) in images" :id="image.id" :index="index" v-bind:key="image.id" :style="'background-image: url(https://huestudios.com/sites/camila.life/content/thumbnail/600/600/crop/' + image.name +')'">
-      </li>
-    </ul>
-    <a class="uk-position-center-left uk-position-large uk-light" href="#" uk-slidenav-previous uk-slider-item="previous"></a>
-    <a class="uk-position-center-right uk-position-large uk-light" href="#" uk-slidenav-next uk-slider-item="next"></a>
-    <ul class="uk-slider-nav uk-dotnav uk-flex uk-flex-center uk-dark"></ul>
+  <div class="uk-width-1-1">
+    <div class="uk-position-relative uk-visible-toggle top-section recipe-images" uk-slider="center: true; animation: pull" v-if="images.length > 0">
+      <ul class="uk-slider-items uk-grid">
+        <li class="slider-item uk-width-1-1" v-for="(image, index) in images" :id="image.id" :index="index" v-bind:key="image.id" :style="'background-image: url(https://huestudios.com/sites/camila.life/content/thumbnail/600/600/crop/' + image.name +')'">
+        </li>
+      </ul>
+      <a class="uk-position-center-left uk-position-large uk-light" href="#" uk-slidenav-previous uk-slider-item="previous"></a>
+      <a class="uk-position-center-right uk-position-large uk-light" href="#" uk-slidenav-next uk-slider-item="next"></a>
+      <ul class="uk-slider-nav uk-dotnav uk-flex uk-flex-center uk-dark"></ul>
+    </div>
+  </div>
+  <div class="uk-width-1-1 uk-width-5-6@m uk-text-center recipe-category" v-if="recipe.category">
+    <h3 v-if="recipe.category == 'Easy'"><span class="condensed-bold title">{{recipe.name}}</span> is an <span class="black-bg green condensed-bold category">EASY</span><span class="uk-visible@s">, familiar</span> recipe made with <span class="uk-visible@s">healthy</span> vegan ingredients!!</h3>
+    <h3 v-if="recipe.category == 'Radical'"><span class="condensed-bold title">{{recipe.name}}</span> is a <span class="black-bg pink condensed-bold category">RADICAL</span>, <span class="uk-visible@s">warrior-style,</span> vegan recipe that challenges!!</h3>
+    <vk-grid class="uk-flex uk-flex-center uk-flex-middle">
+      <div id="recipe-buttons">
+        <button class="uk-button uk-button-default condensed-bold"><vk-icon icon="plus"></vk-icon> SAVE RECIPE</button>
+        <button @click.prevent="show = true" class="uk-button uk-button-default condensed-bold"><vk-icon icon="heart"></vk-icon> SHARE THE LOVE</button>
+      </div>
+    </vk-grid>
+    <div class="uk-text-justify recipe-caption" v-html="recipe.caption"></div>
+  </div>
+  <div class="uk-width-5-6">
+    <vk-grid class="uk-flex uk-flex-center">
+      <div class="recipe-stats-box">
+
+        <div class="uk-position-relative recipe-stats">
+          <h3 class="condensed-bold pink">STATS</h3>
+          <h5> <span v-if="recipe.prep_time" class="stats-item">PREP TIME: <span class="pink condensed-bold">{{recipe.prep_time}} MIN</span></span>
+
+      <span v-if="recipe.cook_time" class="stats-item">COOK TIME: <span class="pink condensed-bold">{{recipe.cook_time}} MIN</span></span>
+
+      <span class="stats-item"><span class="pink condensed-bold">{{recipe.recipe_ingredients.data.length}}</span> INGREDIENTS</span>
+
+      <span class="stats-item"><span class="pink condensed-bold">{{recipe.recipe_directions.data.length}}</span> STEPS</span></h5>
+        </div>
+      </div>
+    </vk-grid>
   </div>
 
-
-    <div class="uk-width-5-6 recipe-caption" v-html="recipe.caption"></div>
-    <div class="uk-width-auto recipe-stats">
-      <vk-grid class="uk-flex uk-flex-center uk-flex-middle">
-      <h5 v-if="recipe.prep_time">PREP TIME: <span class="pink condensed-bold">{{recipe.prep_time}} MIN</span></h5>
-      <h5 v-if="recipe.cook_time">COOK TIME: <span class="pink condensed-bold">{{recipe.cook_time}} MIN</span></h5>
-      <h5><span class="pink condensed-bold">{{recipe.recipe_ingredients.data.length}}</span> INGREDIENTS</h5>
-      <h5><span class="pink condensed-bold">{{recipe.recipe_directions.data.length}}</span> STEPS</h5>
-    </vk-grid>
-    </div>
-    <div class="uk-width-5-6 recipe-tabs">
-      <vk-tabs>
-        <vk-tabs-item :title="'HOW TO GET IT DONE ['+recipe.recipe_directions.data.length+' STEPS]'">
-          <div v-for="direction in recipe.recipe_directions.data" v-bind:key="direction.id" class="recipe-direction">
-            <p>{{direction.title}}</p>
-            <span v-html="direction.content"></span>
+  <div class="uk-width-1-1 uk-width-5-6@s recipe-tabs">
+    <ul class="uk-child-width-expand" uk-switcher="animation: uk-animation-slide-left-small, uk-animation-slide-right-small" uk-tab>
+      <li><a @click.prevent>HOW TO GET IT DONE <span class="uk-visible@s">[{{recipe.recipe_directions.data.length}} STEPS]</span></a></li>
+      <li><a @click.prevent>{{recipe.recipe_ingredients.data.length}}  Ingredients</a></li>
+    </ul>
+    <ul class="uk-switcher" uk-height-match>
+      <li class="recipe-tab">
+        <vk-grid class="uk-flex uk-flex-middle uk-flex-center">
+          <div class="uk-width-1-1 uk-text-center">
+          <h4 class="uk-margin-remove-bottom">Completed <span class="script-font pink">{{currentStep}}</span> of <span class="script-font pink">{{totalSteps}}</span> steps.</h4>
+          <p class="script-font uk-margin-small-bottom uk-margin-remove-top">{{stepMessage}}</p>
+        </div>
+        <div class="uk-width-1-1 uk-width-auto@s uk-margin-remove-top">
+          <div v-for="(direction, index) in recipe.recipe_directions.data" v-bind:key="direction.id" class="recipe-direction">
+            <input class="uk-checkbox" :id="'checkbox-' + index" type="checkbox" @click="stepComplete(index)">
+            <p class="uk-text-capitalize title"><span class="condensed-bold pink uk-text-uppercase" :id="'step-' + index">STEP {{index + 1}}: </span> <span class="title-word" :id="'title-' + index">{{titleCase(direction.title)}}</span></p>
+            <div class="content" :id="'content-' + index" v-html="direction.content"></div>
           </div>
-        </vk-tabs-item>
-          <vk-tabs-item :title="recipe.recipe_ingredients.data.length + ' Ingredients'">
-            Content
-          </vk-tabs-item>
+          </div>
+        </vk-grid>
+      </li>
+      <li class="recipe-tab">
+        <!-- <div v-for="ingredient in ingredients" v-bind:key="ingredient.id">
+          <span v-if="ingredient.measurement">{{ingredient.measurement}}</span>
+          <span v-if="ingredient.measurement_label">{{ingredient.measurement_label}}</span>
+          <span v-if="ingredient.recipe_ingredient_description">{{ingredient.recipe_ingredient_description}}</span>
+        </div>
+        <ingredient v-for="(ingredient, index) in ingredients" :index="index" v-bind:key="ingredient.id" :id="ingredient.ingredient" :recipe="ingredient.recipe" :measurement="ingredient.measurement" :label="ingredient.measurement_label" :description="ingredient.recipe_ingredient_description"></ingredient> -->
+      </li>
 
-      </vk-tabs>
-    </div>
-    <div class="uk-width-1-1">
-      <h1 class="uk-text-center">MORE <span v-if="recipe.category" class="black-bg" :class="recipe.category">{{recipe.category}}</span> <span v-if="recipe.cuisine" class="" > {{recipe.cuisine}}</span> RECIPES</h1>
-    </div>
+    </ul>
+  </div>
+  <div class="uk-width-1-1">
+    <h1 class="uk-text-center">MORE <span v-if="recipe.category" class="black-bg" :class="recipe.category">{{recipe.category}}</span> <span v-if="recipe.cuisine" class="" > {{recipe.cuisine}}</span> RECIPES</h1>
+  </div>
+  <vk-modal center size="container" :show.sync="show" class="uk-padding-remove uk-margin-remove-top full-image">
+    <vk-modal-close @click="show = false"></vk-modal-close>
+
+  </vk-modal>
 </vk-grid>
 </template>
 
 <script>
 import axios from 'axios'
-
+import ingredient from '~/components/recipeIngredient.vue'
 
 export default {
   auth: false,
+  // transition(to, from) {
+  //   if (from && (from.name == 'index' || from.name == 'list' || from.name == 'vegan-plant-based-recipes')) {
+  //     return 'slide-left'
+  //   }
+  // },
   scrollToTop: true,
   async asyncData({
     params,
-    req
+    req,
+    app
   }) {
     return axios.get('https://huestudios.com/sites/camila.life/content/api/1.1/tables/recipes/rows/?filters[url][eq]=' + params.slug).then(res => {
       return {
         recipe: res.data.data[0],
         images: res.data.data[0].images.data,
+        ingredients: res.data.data[0].recipe_ingredients.data
         // name: req ? 'server' : 'client'
       }
     }).catch(function (error) {
@@ -76,26 +125,34 @@ export default {
   },
   data() {
     return {
-      recipe: '',
+      ingredients: [],
       love: false,
-      style: []
+      style: [],
+      show: false,
+      currentStep: 0,
+      nextStep: 1,
+      totalSteps: 0,
+      stepMessage: 'Time to try something new!'
     }
   },
-  components: {},
+  components: {
+    ingredient
+  },
   created() {
-    console.log(this.recipe.recipe_directions.data)
     if (this.recipe.style) {
       this.style = this.makeArray(this.recipe.style)
     }
+    console.log(this.ingredients)
+    this.totalSteps = this.recipe.recipe_directions.data.length
     // if(process.server) {
     //   this.request = 'server'
     // }
   },
-  // head() {
-  //   return {
-  //     title: this.recipe.name
-  //   }
-  // },
+  head() {
+    return {
+      title: this.recipe.name
+    }
+  },
   methods: {
     findLove(str) {
       if (str.includes('LOVE')) {
@@ -107,6 +164,85 @@ export default {
     makeArray(str) {
       var newArray = str.split(',')
       return newArray
+    },
+    titleCase(str) {
+      return str.toLowerCase().split(' ').map(function (word) {
+        return word.replace(word[0], word[0].toUpperCase());
+      }).join(' ');
+    },
+    stepComplete(index) {
+      var element1 = document.getElementById('title-' + index)
+      var element2 = document.getElementById('content-' + index)
+      var element3 = document.getElementById('step-' + index)
+      var element4 = document.getElementById('checkbox-' + index)
+
+      this.nextStep = index + 2;
+      this.currentStep = index + 1;
+      element1.classList.toggle('strike-through')
+      element2.classList.toggle('strike-through')
+      element3.classList.toggle('change-to-grey')
+      // console.log(this.currentStep + " to next: " + this.nextStep)
+
+      if(element4.checked && (this.currentStep == 1)) {
+        this.stepMessage = "Now you're cookin'!"
+      }
+      if(element4.checked && (this.currentStep == (this.totalSteps - 1))) {
+        this.stepMessage = "Only one more step..."
+      }
+      if(!element4.checked && (this.currentStep !== 1)) {
+        console.log(this.currentStep + " it's unchecked dummy")
+        this.currentStep = index;
+        this.nextStep = index + 1;
+        this.stepMessage = "oops...I see you're going backwards."
+
+      } else if (element4.checked && (this.currentStep !== 0) && (this.currentStep !== (this.totalSteps - 1)) && (this.currentStep !== this.recipe.recipe_directions.data.length)) {
+        console.log(this.currentStep + "it's checked")
+        this.stepMessage = "Now you're cookin'!"
+      } else if (!element4.checked && (this.currentStep == 1)) {
+        console.log(this.currentStep + " then it should be")
+        this.currentStep = index;
+        this.nextStep = index + 1;
+        console.log(this.currentStep + " now.")
+        this.stepMessage = 'Time to start again!'
+      }
+
+
+      // last step notification
+      if (((index + 1) == this.recipe.recipe_directions.data.length) && element4.checked && this.$auth.$state.loggedIn) {
+        this.stepMessage = "You did it " + this.$auth.user.given_name + "!! Now time to eat!";
+        this.showSuccessMsg({
+          title: `<span class='condensed' style='font-size: 20px; line-height: 20px; font-weight: 100;'>GREAT JOB <span class='condensed-bold'>` + this.$auth.user.given_name + `</span>!<br>YOU FINISHED MAKING THE <span class='condensed-bold'>` +
+            this.recipe.name + `</span> RECIPE!</span>`,
+          image: 'https://huestudios.com/sites/camila.life/content/thumbnail/100/100/crop/' + this.images[0].name
+        })
+      } else if (((index + 1) == this.recipe.recipe_directions.data.length) && element4.checked) {
+        this.stepMessage = "You did it!! Now time to eat!"
+        this.showSuccessMsg({
+          title: `<span class='condensed' style='font-size: 20px; line-height: 20px; font-weight: 100;'>GREAT JOB!<br>YOU FINISHED MAKING THE <span class='condensed-bold'>` + this.recipe.name + `</span> RECIPE!</span>`,
+          image: 'https://huestudios.com/sites/camila.life/content/thumbnail/100/100/crop/' + this.images[0].name
+        })
+      }
+    }
+  },
+  notifications: {
+    showSuccessMsg: {
+      type: 'success',
+      title: 'successfully added',
+      position: 'bottomRight',
+      color: 'white',
+      message: '',
+      timeout: 5000,
+      cb: function () {},
+      onClosing: function () {
+        // var listTotalBadge = document.getElementById('grocery-list-count')
+        // listTotalBadge.classList.remove('pulseEffect')
+      },
+      onClosed: function () {
+        // var listTotalBadge = document.getElementById('grocery-list-count')
+        // setTimeout(function () {
+        //   listTotalBadge.classList.add('pulseEffect')
+        // }, 200)
+      }
     }
   }
 }
